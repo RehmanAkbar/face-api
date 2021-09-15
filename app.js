0ca4d4a9e290c9234021785b2133a9531b8dab4b
Rehman Akbar <15936680+RehmanAkbar@users.noreply.github.com>
2021-09-16 00:30:27 +0500

    initial

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
 const faceApiService = require("./faceApiService");
-
+var cors = require('cors')
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(fileUpload());
+app.use(cors());
 
 app.post("/upload", async (req, res) => {
-  const { file } = req.files;
 
-  const result = await faceApiService.detect(file.data, file.name);
+  if (!req.files)
+    return res.status(400).json({ message: 'No files were uploaded.' });
+
+  var file = req.files.reference;
+  
+  const result = await faceApiService.recognition(req.files);
+  // return result;
+  // const result = await faceApiService.detect(file.data, file.name);
 
   res.json({
-    detectedFaces: result.length,
-    url: `http://localhost:3000/out/${file.name}`,
+    result
+    // url: `http://localhost:3000/out/${file.name}`,
   });
 });
 
--- faceapiService.js
+++ faceapiService.js
@@ -22,10 +22,12 @@ async function image(file) {
   return result;
 }
 
+
 async function detect(tensor) {
   const result = await faceapi.detectAllFaces(tensor, optionsSSDMobileNet);
   return result;
 }
+
 
 async function main(file, filename) {
   console.log("FaceAPI single-process test");
@@ -36,14 +38,15 @@ async function main(file, filename) {
   await faceapi.tf.ready();
 
   console.log(
-    `Version: TensorFlow/JS ${faceapi.tf?.version_core} FaceAPI ${
-      faceapi.version.faceapi
+    `Version: TensorFlow/JS ${faceapi.tf?.version_core} FaceAPI ${faceapi.version.faceapi
     } Backend: ${faceapi.tf?.getBackend()}`
   );
 
   console.log("Loading FaceAPI models");
   const modelPath = path.join(__dirname, modelPathRoot);
   await faceapi.nets.ssdMobilenetv1.loadFromDisk(modelPath);
+
+
   optionsSSDMobileNet = new faceapi.SsdMobilenetv1Options({
     minConfidence: 0.5,
   });
@@ -63,6 +66,63 @@ async function main(file, filename) {
   return result;
 }
 
+
+async function recognition(files) {
+  console.log("FaceAPI single-process test");
+
+  await faceapi.tf.setBackend("tensorflow");
+  await faceapi.tf.enableProdMode();
+  await faceapi.tf.ENV.set("DEBUG", false);
+  await faceapi.tf.ready();
+
+  // console.log(
+  //   `Version: TensorFlow/JS ${faceapi.tf?.version_core} FaceAPI ${faceapi.version.faceapi
+  //   } Backend: ${faceapi.tf?.getBackend()}`
+  // );
+
+  // console.log("Loading FaceAPI models");
+  const modelPath = path.join(__dirname, modelPathRoot);
+  await faceapi.nets.ssdMobilenetv1.loadFromDisk(modelPath);
+  await faceapi.nets.faceLandmark68Net.loadFromDisk(modelPath)
+  await faceapi.nets.faceRecognitionNet.loadFromDisk(modelPath)
+
+  const REFERENCE_IMAGE = files.reference.data
+  const QUERY_IMAGE = files.query.data
+
+  const referenceImage = await canvas.loadImage(REFERENCE_IMAGE)
+  const queryImage = await canvas.loadImage(QUERY_IMAGE)
+
+  optionsSSDMobileNet = new faceapi.SsdMobilenetv1Options({
+    minConfidence: 0.5,
+  });
+
+  const resultsRef = await faceapi.detectAllFaces(referenceImage, optionsSSDMobileNet)
+    .withFaceLandmarks()
+    .withFaceDescriptors()
+
+  const resultsQuery = await faceapi.detectAllFaces(queryImage, optionsSSDMobileNet)
+    .withFaceLandmarks()
+    .withFaceDescriptors()
+
+  const faceMatcher = new faceapi.FaceMatcher(resultsRef)
+
+  const labels = faceMatcher.labeledDescriptors.map(ld => ld.label)
+  // const refDrawBoxes = resultsRef.map(res => res.detection.box).map((box, i) => new faceapi.draw.DrawBox(box, { label: labels[i] }))
+  // const outRef = faceapi.createCanvasFromMedia(referenceImage)
+  // refDrawBoxes.forEach(drawBox => drawBox.draw(outRef))
+  const queryDrawBoxes = resultsQuery.map(res => {
+    const bestMatch = faceMatcher.findBestMatch(res.descriptor)
+    // return res.detection.box, { label: bestMatch.toString() }
+    return { box: res.detection.box, label: bestMatch.toString() };
+    return new faceapi.draw.DrawBox(res.detection.box, { label: bestMatch.toString() })
+  })
+  return queryDrawBoxes;
+  // console.log(queryDrawBoxes)
+  // console.log(resultsRef);
+  // return resultsRef;
+}
+
 module.exports = {
   detect: main,
+  recognition: recognition
 };
