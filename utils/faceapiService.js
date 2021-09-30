74b7ea2c070c443444f7e999b7f993122cd55222
Rehman Akbar <15936680+RehmanAkbar@users.noreply.github.com>
2021-09-30 23:45:11 +0500

    dispose image

--- utils/faceapiService.js
+++ utils/faceapiService.js
@@ -105,11 +105,11 @@ async function recognition(files) {
     .withFaceDescriptors()
 
   if (!resultsRef.length) {
-    tensor.dispose();
+    resultsRef.dispose();
     return ['no face detected'];
   }
   if (!resultsQuery.length) {
-    tensor.dispose();
+    resultsQuery.dispose();
     return ['no face detected'];
   }
 
@@ -125,7 +125,8 @@ async function recognition(files) {
     return { box: res.detection.box, label: bestMatch.toString() };
     return new faceapi.draw.DrawBox(res.detection.box, { label: bestMatch.toString() })
   })
-  tensor.dispose();
+  resultsQuery.dispose();
+  resultsRef.dispose();
   return queryDrawBoxes;
   // console.log(queryDrawBoxes)
   // console.log(resultsRef);
